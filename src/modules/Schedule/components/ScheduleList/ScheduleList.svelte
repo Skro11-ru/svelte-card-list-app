9aed2cdf940b15b39227eb8ab2449a6d1c6e53ca
Дмитрий Бунецкий <bunetskydn@gmail.com>
2022-08-12 16:26:57 +0700

    feat: implement a component for displaying a list of items and displaying a schedule

---
+++ src/modules/Schedule/components/ScheduleList/ScheduleList.svelte
@@ -0,0 +1,29 @@
+<ul class="schedule-list">
+  {#each list as item}
+    <li class="schedule-list__item">
+      <ScheduleItem item="{item}" (item.id) />
+    </li>
+  {/each}
+</ul>
+
+<script lang="ts">
+  import ScheduleItem from '@/modules/Schedule/components/ScheduleItem/ScheduleItem.svelte';
+  export let list;
+</script>
+
+<style lang="scss">
+  .schedule-list {
+    &__item {
+      border: 1px solid $color-rhythm;
+
+      padding: 10px;
+      @include border(5px);
+      &:not(:last-child) {
+        margin-bottom: 10px;
+      }
+      &:hover {
+        @include shadow();
+      }
+    }
+  }
+</style>
---
+++ src/modules/Schedule/view/ScheduleView.svelte
@@ -0,0 +1,27 @@
+<section class="schedule">
+  <h1>Расписание</h1>
+  <ScheduleList list="{scheduleList}" />
+</section>
+
+<script lang="typescript">
+  import { onMount } from 'svelte';
+  import ScheduleList from '@/modules/Schedule/components/ScheduleList/ScheduleList.svelte';
+  import { ScheduleModule } from '@/modules/Schedule/services/ScheduleServices';
+  import type { IScheduleItem } from '@/modules/Schedule/interfaces/ScheduleInterfaces';
+
+  onMount(async () => {
+    const res = await ScheduleModule.getSchedule();
+    scheduleList = [...res];
+  });
+
+  //Variables
+  let scheduleList: IScheduleItem[] = [];
+</script>
+
+<style lang="scss">
+  .schedule {
+    max-width: 600px;
+    padding: 20px;
+    margin: auto;
+  }
+</style>
