b9c7f5655770b1dbf976d27743769fd9105a3bc3
Дмитрий Бунецкий <bunetskydn@gmail.com>
2022-08-12 11:07:45 +0700

    feat: change project configuration settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { resolve } from 'path';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
@@ -12,13 +11,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': resolve(__dirname, '/src'),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/assets/styles/cfg-scss";`,
-      },
     },
   },
 })
