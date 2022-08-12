04a0ebbf60141c6ccc0f6b0ae1f1713c64b891ef
Дмитрий Бунецкий <bunetskydn@gmail.com>
2022-08-12 16:16:26 +0700

    feat: complete the implementation of the schedule

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
+<TimeTableView />
 
 <script lang="ts">
+  import TimeTableView from '@/modules/Schedule/view/ScheduleView.svelte';
 </script>
 
 <style lang="scss">
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [svelte()],
   server: {
     port:4000,
