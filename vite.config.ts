9f54296cb8a30966b09a036929b1a3ad90c77430
Дмитрий Бунецкий <bunetskydn@gmail.com>
2022-08-14 19:35:44 +0700

    fix: fix alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   server: {
-    port:4000,
-    host: true
+    port: 4000,
+    host: true,
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, '/src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
